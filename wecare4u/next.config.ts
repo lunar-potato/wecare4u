dcd25ecc392f9e3a96378899880bb1a8d669351b
Gwyneth <118124645+lunar-potato@users.noreply.github.com>
2025-02-18 22:11:07 +0000

    Trying to fix error 500 im going crazy

--- wecare4u/next.config.ts
+++ wecare4u/next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  },
 };
 
 export default nextConfig;
